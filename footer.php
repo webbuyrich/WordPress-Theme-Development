adb4a6801ed0e2ad894da71d7d557ae0347c94b5
webbuyrich@gmail.com <webbuyrich@gmail.com>
2019-10-13 06:32:36 -0700

    added grid to footer. added word break to long text

--- footer.php
+++ footer.php
@@ -14,19 +14,24 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrapwp' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bootstrapwp' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrapwp' ), 'bootstrapwp', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="container">
+			<div class="row">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrapwp' ) ); ?>">
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'bootstrapwp' ), 'WordPress' );
+						?>
+					</a>
+					<span class="sep"> | </span>
+						<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrapwp' ), 'bootstrapwp', '<a href="http://underscores.me/">Underscores.me</a>' );
+						?>
+				</div><!-- .site-info -->
+			</div>
+		</div>
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
