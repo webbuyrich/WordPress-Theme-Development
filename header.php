3cb490548bf104d415ea037da7bcfcc5f8c074ca
webbuyrich@gmail.com <webbuyrich@gmail.com>
2019-10-13 05:59:23 -0700

    added bootstrap grid to index page and sidebar

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bootstrapwp' ); ?></a>
+	<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bootstrapwp' ); ?></a> -->
 
 	<header id="masthead" class="site-header">
 	<nav role="navigation">
--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,9 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area">
+<aside id="secondary" class="widget-area col-md-3 col-lg-3">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
+
+	</div><!-- #row -->
+</div><!-- #container -->
